3e383c758ad5d68e2c2b1f37261daac81fde8611
asato <futail2a@gmail.com>
2016-08-10 15:15:11 +0900

    create PathFollowerDecorator.class

--- src/PathFollowerSVC_impl.java
+++ src/PathFollowerSVC_impl.java
@@ -19,43 +19,19 @@ public class PathFollowerSVC_impl extends PathFollowerPOA{
      * Methods corresponding to IDL attributes and operations
      */
     public RTC.RETURN_VALUE followPath(RTC.Path2D path) {
-        System.out.print("followPath calling");
+        System.out.println("followPath calling");
         return RTC.RETURN_VALUE.RETVAL_OK ;
     }
 
     public RTC.RETURN_VALUE getState(RTC.FOLLOWER_STATEHolder state) {
-        System.out.print("getState calling");
+        System.out.println("getState calling");
         return RTC.RETURN_VALUE.RETVAL_OK;
     }
 
     public RTC.RETURN_VALUE followPathNonBlock(RTC.Path2D path) {
-        System.out.print("followPathNonBlock calling");
+        System.out.println("followPathNonBlock calling");
         return RTC.RETURN_VALUE.RETVAL_OK;
     }
 
 //  End of example implementational code
-}
-
-public class PathFollowerDecorator extends PathFollowerSVC_impl{
-	
-	private PathFollowerSVC_impl m_PathFollowerSVC_impl;
-	
-	public PathFollowerDecorator(){
-        System.out.print("Decorating");
-		m_PathFollowerSVC_impl = new PathFollowerSVC_impl();
-	}
-	public RTC.RETURN_VALUE followPath(RTC.Path2D path) {
-        System.out.print("Decorating");
-        return m_PathFollowerSVC_impl.followPath(path);
-    }
-
-    public RTC.RETURN_VALUE getState(RTC.FOLLOWER_STATEHolder state) {
-        System.out.print("Decorating");
-        return m_PathFollowerSVC_impl.getState(state);
-    }
-
-    public RTC.RETURN_VALUE followPathNonBlock(RTC.Path2D path) {
-        System.out.print("Decorating");
-        return m_PathFollowerSVC_impl.followPathNonBlock(path);
-    }
 }
